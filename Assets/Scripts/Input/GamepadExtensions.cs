194893afa910a70e1c385ce6c50e55822a03873e
SurDno <surdno@yandex.ru>
2023-02-23 19:02:48 +0000

    Replaced three dictionaries with a single tuple

--- Assets/Scripts/Input/GamepadExtensions.cs
+++ Assets/Scripts/Input/GamepadExtensions.cs
@@ -7,10 +7,8 @@ using System.Collections.Generic;
 // Holds some extension functions for gamepads that allow to more easily get necessary control values and whether gamepad is connected.
 // Also allows to use devices not labeled as gamepads (such as DualShock controllers that are registered as joysticks) as gamepads by accessing properties on a lower level.
 public static class GamepadExtensions {
-	// A dictionary of dictionaries to get a dictionary of input values by device ID, and then current frame value by button name, last frame value by button name and frame stamp to check if data is up-to-date.
-	static Dictionary<int, Dictionary<string, bool>> currentFramePresses = new Dictionary<int, Dictionary<string, bool>>();
-	static Dictionary<int, Dictionary<string, bool>> lastFramePresses = new Dictionary<int, Dictionary<string, bool>>();
-	static Dictionary<int, Dictionary<string, int>> frameCountForCurrent = new Dictionary<int, Dictionary<string, int>>();
+	// A dictionary of dictionaries of tuples to get info about last frame press and current frame press from gamepad device ID and button name.
+	static Dictionary<int, Dictionary<string, (bool currentFramePresses, bool lastFramePresses, int frameCountForCurrent)>> buttonStates = new Dictionary<int, Dictionary<string, (bool, bool, int)>>();
 	
 	// A value under which input from sticks may be ignored.
 	const float deadzone = 0.05f;
@@ -31,47 +29,33 @@ public static class GamepadExtensions {
 	}
 	
 	// Gets whether the button is currently pressed but was not pressed before or is currently not pressed but was pressed before.
-	public static bool GetButtonChange(InputDevice gamepadInstance, string gamepadName, string joystickName, bool onDown) {
-		if (!IsGamepadConnected(gamepadInstance))
-			return false;
-		
-		// Check if there already are entires for the current frame of that key.
-		if (frameCountForCurrent.ContainsKey(gamepadInstance.deviceId) &&
-			frameCountForCurrent[gamepadInstance.deviceId].ContainsKey(gamepadName)) {
+		public static bool GetButtonChange(InputDevice gamepadInstance, string gamepadName, string joystickName, bool onDown) {
+			if (!IsGamepadConnected(gamepadInstance))
+				return false;
 			
-			// Check if our current frame data for that key is outdated.
-			if(frameCountForCurrent[gamepadInstance.deviceId][gamepadName] != Time.frameCount) {
-				// Move presses to last frame dictionary.
-				lastFramePresses[gamepadInstance.deviceId][gamepadName] = currentFramePresses[gamepadInstance.deviceId][gamepadName];
+			// Check if there already are entires for the current frame of that key.
+			if (buttonStates.ContainsKey(gamepadInstance.deviceId) &&
+				buttonStates[gamepadInstance.deviceId].ContainsKey(gamepadName)) {
 				
-				// Populate dictionaries with current presses and frame amounts.
-				currentFramePresses[gamepadInstance.deviceId][gamepadName] = GetButton(gamepadInstance, gamepadName, joystickName);
-				frameCountForCurrent[gamepadInstance.deviceId][gamepadName] = Time.frameCount;
+				// If our current frame data for that key is outdated, move currentFramePresses to lastFramePresses and populare rest of dictionary with new data.
+				if(buttonStates[gamepadInstance.deviceId][gamepadName].frameCountForCurrent != Time.frameCount)
+					buttonStates[gamepadInstance.deviceId][gamepadName] = (GetButton(gamepadInstance, gamepadName, joystickName), buttonStates[gamepadInstance.deviceId][gamepadName].currentFramePresses, Time.frameCount);
+
+				
+			} else {
+				// If there is no information for that gamepad, populate that data for the first time.
+				if(!buttonStates.ContainsKey(gamepadInstance.deviceId))
+					buttonStates[gamepadInstance.deviceId] = new Dictionary<string, (bool, bool, int)>();
+				
+				// If there is no information for that gamepad key, populate that data for the first time.
+				buttonStates[gamepadInstance.deviceId][gamepadName] = (GetButton(gamepadInstance, gamepadName, joystickName), false, Time.frameCount);
 			}
 			
-		} else {
-			// If there is no information for that gamepad, populate that data for the first time.
-			if(!currentFramePresses.ContainsKey(gamepadInstance.deviceId))
-				currentFramePresses[gamepadInstance.deviceId] = new Dictionary<string, bool>();
-			if(!frameCountForCurrent.ContainsKey(gamepadInstance.deviceId))
-				frameCountForCurrent[gamepadInstance.deviceId] = new Dictionary<string, int>();
-			if(!lastFramePresses.ContainsKey(gamepadInstance.deviceId))
-				lastFramePresses[gamepadInstance.deviceId] = new Dictionary<string, bool>();
+			bool currentlyPressed = buttonStates[gamepadInstance.deviceId][gamepadName].currentFramePresses;
+			bool pressedBefore = buttonStates[gamepadInstance.deviceId][gamepadName].lastFramePresses;
 			
-			// If there is no information for that gamepad key, populate that data for the first time.
-			currentFramePresses[gamepadInstance.deviceId][gamepadName] = GetButton(gamepadInstance, gamepadName, joystickName);
-			frameCountForCurrent[gamepadInstance.deviceId][gamepadName] = Time.frameCount;
-			lastFramePresses[gamepadInstance.deviceId][gamepadName] = false;
+			return onDown ? (currentlyPressed && !pressedBefore) : (!currentlyPressed && pressedBefore);
 		}
-		
-		bool currentlyPressed = currentFramePresses[gamepadInstance.deviceId][gamepadName];
-		bool pressedBefore = lastFramePresses[gamepadInstance.deviceId][gamepadName];
-		
-		if(onDown)
-			return currentlyPressed && !pressedBefore;
-		else
-			return !currentlyPressed && pressedBefore;
-	}
 	
 	// Common function for getting axes (from sticks / d-pad).
 	public static Vector2 GetAxes(InputDevice gamepadInstance, string gamepadNameX, string gamepadNameY, string joystickNameX, string joystickNameY, bool joystickReverseY) {
