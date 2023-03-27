37a0414256e3717e019f5b28ac001ab729e70f86
Ahsanafzali123 <47230996+Ahsanafzali123@users.noreply.github.com>
2023-03-28 00:56:45 +0100

    Timer Has been added
    5 minutes timer has been added once the timer is below 1 minute it will change colour to red and when its 0 it will load the setup scene

--- Assets/Scripts/Gameplay/ScoreManager.cs
+++ Assets/Scripts/Gameplay/ScoreManager.cs
@@ -2,50 +2,107 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
-public class ScoreManager : MonoBehaviour {
+
+public class ScoreManager : MonoBehaviour
+{
+
     [Header("Prefabs and Cached Objects")]
     [SerializeField] private Text leftTeamScoreText;
     [SerializeField] private Text rightTeamScoreText;
-    [Header("Prefabs and Cached Objects")]
-    private int goalsTillVictory = 15;
+    [SerializeField] private Text timerText;
+
+    [Header("Game Settings")]
+    [SerializeField] private int goalsTillVictory = 30;
+    [SerializeField] private int matchDurationSeconds = 300;
 
     [Header("Current Values")]
     private static int leftTeamScore = 0;
     private static int rightTeamScore = 0;
+    private float remainingTime;
 
     private Coroutine timerCoroutine;
 
-    void Update() {
+    void Start()
+    {
+        remainingTime = matchDurationSeconds;
+        UpdateTimerText();
+        StartTimer();
+    }
+
+    void Update()
+    {
         leftTeamScoreText.text = leftTeamScore.ToString();
         rightTeamScoreText.text = rightTeamScore.ToString();
 
-        if (leftTeamScore >= goalsTillVictory || rightTeamScore >= goalsTillVictory) {
-			// Victory cutscene code should go here.
-			
-			GameController.ReturnToMenu();
+        if (leftTeamScore >= goalsTillVictory || rightTeamScore >= goalsTillVictory || remainingTime <= 0f)
+        {
+            EndMatch();
+        }
+
+        // Change the color of the timer text to red if remaining time is less than 1 minute
+        if (remainingTime < 60f)
+        {
+            timerText.color = Color.red;
+        }
+        else
+        {
+            timerText.color = Color.white;
         }
     }
 
-    public static void GoalLeft() {
+    public static void GoalLeft()
+    {
         leftTeamScore++;
         MatchController.Restart();
     }
 
-    public static void GoalRight() {
+    public static void GoalRight()
+    {
         rightTeamScore++;
         MatchController.Restart();
     }
 
-    public static void ResetScoreData() {
+    public static void ResetScoreData()
+    {
         leftTeamScore = 0;
         rightTeamScore = 0;
     }
-	
-	public static bool GetIsRedWinning() {
-		return rightTeamScore > leftTeamScore;
-	}
-	
-	public static bool GetIsGreenWinning() {
-		return leftTeamScore > rightTeamScore;
-	}
+
+    public static bool GetIsRedWinning()
+    {
+        return rightTeamScore > leftTeamScore;
+    }
+
+    public static bool GetIsGreenWinning()
+    {
+        return leftTeamScore > rightTeamScore;
+    }
+
+    private void StartTimer()
+    {
+        timerCoroutine = StartCoroutine(Timer());
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(remainingTime / 60f);
+        int seconds = Mathf.FloorToInt(remainingTime % 60f);
+        timerText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+
+    private IEnumerator Timer()
+    {
+        while (remainingTime > 0f)
+        {
+            remainingTime -= Time.deltaTime;
+            UpdateTimerText();
+            yield return null;
+        }
+    }
+
+    private void EndMatch()
+    {
+        StopCoroutine(timerCoroutine);
+        GameController.ReturnToMenu();
+    }
 }
