404ade46201d904ca65f2fec32d3d7994544c9cb
ChalkySinger <87450356+ChalkySinger@users.noreply.github.com>
2023-03-28 01:34:02 +0100

    Animation code + some materials

--- Assets/Scripts/Gameplay/CharacterAnimation.cs
+++ Assets/Scripts/Gameplay/CharacterAnimation.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterAnimation : MonoBehaviour
-{
-    //public GetMoving();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ {
+	public Animator anim;
+	public bool moving;
+	public bool startCharge;
+	public bool hit;
+	
+	void Update()
+	{
+		// we need to get all necessary values from other scripts firts
+		if(GetComponent<CharacterControls>().GetMoving())
+			moving = true;
+		else
+			moving = false;
+		
+		if(GamepadInput.GetRightTrigger(GetComponent<CharacterOwner>().GetOwner().GetGamepad()))
+			startCharge = true;
+		else
+			startCharge = false;
+		
+		if(GamepadInput.GetRightTrigger(GetComponent<CharacterOwner>().GetOwner().GetGamepad()))
+			hit = false;
+		else
+			hit = true;
+		
+		// And then use it for animation data
+		anim.SetBool("Moving", moving);
+		anim.SetBool("StartCharge", startCharge);
+		anim.SetBool("Hit", hit);
+	}
 }
