0b579219396a241aec6377e70277c3e2e25c89db
SurDno <surdno@yandex.ru>
2023-04-01 00:51:05 +0100

    Fog small update

--- Assets/Scripts/GroundFog.cs
+++ Assets/Scripts/GroundFog.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
 
-public class GroundFog : MonoBehaviour
-{
-    public Color fogColor = Color.gray; // Set the color of the fog
-    public float density = 0.02f; // Set the density of the fog
-    public float startDistance = 0.1f; // Set the start distance of the fog
-    public float endDistance = 20f; // Set the end distance of the fog
+public class GroundFog : MonoBehaviour {
+    [SerializeField]private Color fogColor = Color.gray; 
+    [SerializeField]private float density = 0.02f; 
+    [SerializeField]private float startDistance = 0.1f; 
+    [SerializeField]private float endDistance = 20f; 
+	[SerializeField]private FogMode fogMode = FogMode.Linear;
 
-    private void Update()
-    {
-        RenderSettings.fog = true; // Enable fog
-        RenderSettings.fogColor = fogColor; // Set fog color
-        RenderSettings.fogDensity = density; // Set fog density
-        RenderSettings.fogMode = FogMode.Linear; // Set fog mode to linear
-        RenderSettings.fogStartDistance = startDistance; // Set fog start distance
-        RenderSettings.fogEndDistance = endDistance; // Set fog end distance
+	// Enable fog.
+	private void Start() {
+        RenderSettings.fog = true; 
+		// In build, all fog settings are applied on Scene start.
+		#if UNITY_STANDALONE
+        RenderSettings.fogColor = fogColor; 
+        RenderSettings.fogDensity = density;
+        RenderSettings.fogMode = fogMode; 
+        RenderSettings.fogStartDistance = startDistance; 
+        RenderSettings.fogEndDistance = endDistance; 
+		#endif
+	}
+	
+    private void Update() {
+		// In editor, fog settings are applied every frame for easier tweaking during play.
+		#if UNITY_EDITOR
+        RenderSettings.fogColor = fogColor; 
+        RenderSettings.fogDensity = density;
+        RenderSettings.fogMode = fogMode; 
+        RenderSettings.fogStartDistance = startDistance; 
+        RenderSettings.fogEndDistance = endDistance; 
+		#endif
     }
 }
