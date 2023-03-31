c517c12d4344c1ab4f45b9cbbe5155c9b97a3b94
Ahsanafzali123 <47230996+Ahsanafzali123@users.noreply.github.com>
2023-03-31 04:17:20 +0100

    Added Fog to the maps and bloom code
    the fog is being controlled by code
    and the bloom is by the camera

---
+++ Assets/GroundFog.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GroundFog : MonoBehaviour
+{
+    public Color fogColor = Color.gray; // Set the color of the fog
+    public float density = 0.02f; // Set the density of the fog
+    public float startDistance = 0.1f; // Set the start distance of the fog
+    public float endDistance = 20f; // Set the end distance of the fog
+
+    private void Start()
+    {
+        RenderSettings.fog = true; // Enable fog
+        RenderSettings.fogColor = fogColor; // Set fog color
+        RenderSettings.fogDensity = density; // Set fog density
+        RenderSettings.fogMode = FogMode.Linear; // Set fog mode to linear
+        RenderSettings.fogStartDistance = startDistance; // Set fog start distance
+        RenderSettings.fogEndDistance = endDistance; // Set fog end distance
+    }
+}
